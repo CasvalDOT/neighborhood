e784036902f34d2dd0fa1ccf12b805a288ca3146
Casval <fabrizio.gortani@protonmail.com>
2021-01-09 21:56:55 +0100

    feat: add man help function

---
+++ helpers/man.go
@@ -0,0 +1,39 @@
+package helpers
+
+import (
+	"bytes"
+	"fmt"
+	"neighborhood/config"
+	"text/template"
+)
+
+var tmpl = `
+{{.Name}}
+--------------------------------------------
+A very simple portknocker!!
+
+USAGE
+{{.Command}} <host> <port:protocol> <port:protocol> <options>
+
+OPTIONS
+{{range .Options}}
+-{{.Flag}} {{.Description}}
+{{end}}
+
+EXAMPLE
+{{.Command}} 192.168.10.10 3000:TCP 4000:UDP 5000
+`
+
+// Man : show the manual
+func Man() {
+	t, err := template.New("help").Parse(tmpl)
+	if err != nil {
+		panic(err)
+	}
+
+	var tBuffer bytes.Buffer
+	t.Execute(&tBuffer, config.Config)
+
+	fmt.Println(string(tBuffer.Bytes()))
+
+}
